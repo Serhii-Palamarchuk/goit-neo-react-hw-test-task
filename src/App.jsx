acca21dfba544550a284257c8450ce41900a3ac3
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-06-09 01:06:56 +0300

    fix: Update loading logic to show Loader only during detail loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Navbar from "./components/Navbar";
 import Loader from "./components/Loader";
+import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import CatalogPage from "./pages/CatalogPage";
 import FavoritesPage from "./pages/FavoritesPage";
 import CamperDetailPage from "./pages/CamperDetailPage";
 
 function App() {
-  const loading = useSelector(
-    (state) =>
-      state.campers.status === "loading" ||
-      state.campers.detailStatus === "loading"
+  // Покажемо full-screen Loader лише під час завантаження деталей
+  const detailLoading = useSelector(
+    (state) => state.campers.detailStatus === "loading"
   );
 
   return (
     <>
       <Navbar />
-      {loading && <Loader />}
+      {detailLoading && <Loader />}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
