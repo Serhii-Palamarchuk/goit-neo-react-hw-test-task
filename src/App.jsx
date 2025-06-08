d729e5dbda2c5fff185109195e78603c837b75bc
Serhii-Palamarchuk <serhiy.palamarchuk@gmail.com>
2025-06-09 00:00:13 +0300

    feat: Add Loader component and integrate loading state in App for improved user experience style: Update index.html with appropriate meta tags and title for better SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+// src/App.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import HomePage from "./pages/HomePage";
 import CatalogPage from "./pages/CatalogPage";
 import CamperDetailPage from "./pages/CamperDetailPage";
+import Loader from "./components/Loader";
 
 function App() {
+  const loading = useSelector(
+    (state) =>
+      state.campers.status === "loading" ||
+      state.campers.detailStatus === "loading"
+  );
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/catalog" element={<CatalogPage />} />
-      <Route path="/catalog/:id" element={<CamperDetailPage />} />
-    </Routes>
+    <>
+      {loading && <Loader />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/catalog/:id" element={<CamperDetailPage />} />
+      </Routes>
+    </>
   );
 }
 
